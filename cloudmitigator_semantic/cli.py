84954624ed1e51dd6b83b4331ebe325291eb5fd4
Michael Schappacher <michael.schappacher@stelligent.com>
2020-02-05 17:51:10 -0500

    patch: add in help information into the cli for reference.

--- cloudmitigator_semantic/cli.py
+++ cloudmitigator_semantic/cli.py
@@ -11,12 +11,14 @@ def semantic():
     pass
 
 
-@semantic.command("changed")
+@semantic.command("changed", short_help="Return the latest git tag version if it has been "
+                                        "changed, or the current git tag version if unchanged.")
 def changed():
     sys.stdout.write(str(git_actions.version.version_changed))
 
 
-@semantic.command("version")
+@semantic.command("version", short_help="Return a boolean on whether or not the version has been "
+                                        "incremented.")
 def version():
     sys.stdout.write(str(git_actions.version.version))
 
